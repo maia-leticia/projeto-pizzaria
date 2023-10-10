fd9ee6b5016636e90dba31652cbc320e48395265
maia-leticia <leticiammaia2017@gmail.com>
2023-10-10 15:21:07 -0300

    criei funcionalidades

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+// Funções 
+
+
 pizzaJson.map((item, index) => {
-    // copiei o html e adicionei a quantidade de itens na lista
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+    // Colocar pizzas
+    let pizzaItem = document.querySelector('.pizza-item').cloneNode(true)
     document.querySelector('.pizza-area').append(pizzaItem)
     // dados de cada pizza
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+
+    // ABRIR janela modal
+    pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
+        e.preventDefault()
+        document.querySelector('.pizzaWindowArea').style.display = 'flex'
+    // Preenchimento dos dados
+    document.querySelector('.pizzaBig img').src = item.img
+    document.querySelector('.pizzaInfo h1').innerHTML = item.name
+    document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price}`
+    document.querySelector(".pizzaInfo--desc").innerHTML = item.description
+    })
+
+    // FECHAR janela modal
+    document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', (f) => {
+        f.preventDefault()
+        document.querySelector('.pizzaWindowArea').style.display = 'none'
+    })
+    document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', (f) => {
+        f.preventDefault()
+        document.querySelector('.pizzaWindowArea').style.display = 'none'
+    })
 })
